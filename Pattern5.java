1c88baaf0017704d49c53bd31e90edf6c1ded2f5
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-20 23:49:35 +0530

    Pattern5

---
+++ Pattern5.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+
+public class Pattern5 {
+	public static void main(String[] args)
+	{
+		Scanner scn = new Scanner(System.in);
+		System.out.println("enter a number");
+		int n=scn.nextInt();
+		int row,col,val;
+		row=1;
+		val=1;
+		while(row<=n)
+		{
+			col=1;
+			while(col<=row)
+			{
+				System.out.print(val);
+				col=col+1;
+			}
+			val++;
+			System.out.println();
+			row=row+1;
+		}
+	}
+
+}
