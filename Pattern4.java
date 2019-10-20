0e11377004fb6b935e75124a27d07fc0161263a1
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-20 23:49:11 +0530

    Pattern4

---
+++ Pattern4.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+
+public class Pattern4 {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=scn.nextInt();
+		int row,col,val;
+		row=1;
+		val=1;
+		while(row<=n)
+		{
+			col=1;
+			while(col<=row)
+			{
+			if(col==1 || col==row)
+			{
+				System.out.print(" "+val+" ");
+			}
+			else
+			{
+				System.out.print(" 0 ");
+			}
+			col++;
+			}
+			System.out.println();
+			row=row+1;
+			val++;
+		}
+		
+	}
+
+}
