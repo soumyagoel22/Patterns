6d276badf6b20365bff15adbc4e5f8b096cb6f6c
soumyagoel22 <34788632+soumyagoel22@users.noreply.github.com>
2019-10-21 23:30:09 +0530

    Pattern6

---
+++ Pattern6.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class Pattern6 {
+	public static void main(String[] args)
+	{
+		Scanner scn=new Scanner(System.in);
+	    System.out.println("enter a number");
+	    int n=scn.nextInt();
+	    int row,col,val;
+	    row=1;
+	    while(row<=n)
+	    {
+	        col=1;
+	        while(col<=n-row)
+	        {
+	        	System.out.print(" ");
+	        	col=col+1;
+	        }
+	        col=1;
+	        val=row;
+	        while(col<=row)
+	        {
+	        	System.out.print(val);
+	        	val=val+1;
+	        	col=col+1;
+	        }
+	        val=val-2;
+	        col=1;
+	        while(col<=row-1)
+	        {
+	        	System.out.print(val);
+	        	val=val-1;
+	        	col=col+1;
+	        }
+	        System.out.println();
+	        row=row+1;
+	    }
+	}
+
+}
